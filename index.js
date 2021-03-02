b103d8482068c897922c4e56172a853dc00123ca
Najmah A. Omar <omar.na18@s.msumain.com.edu.ph>
2021-03-02 10:04:12 +0800

    schedule-interview

--- index.js
+++ index.js
@@ -6,17 +6,26 @@ var studentStatus = ['Applying', 'Under Interview', 'Exam Pending', 'Admitted', 
 
 (async function() {
     await acceptStudent('201811942', 'Anisah I. Dayaan', 21, 'Marawi City LDS');
+    var scheduleDate = 'February 28, 2021';
+    await scheduleInterview('201811942', scheduleDate);
 }());
-
 
 function connectToDatabase(db){
     return level(db, { valueEncoding: 'json' });   
 }
 
 async function acceptStudent(id, fullName, age, address){
-    
-    console.log('Student Admission');
+    console.log('\nStudent Admission');
     const student = { ID: id, Name: fullName, Age: age, Address: address, Status : studentStatus[0]};
+    await db.put(id, student);
+    console.log(student);
+}
+
+async function scheduleInterview(id, scheduleDate){
+    var student = await db.get(id);
+    console.log('\nSchedule of the Interview');
+    student.Status = studentStatus[1];
+    student.InterviewSchedule = scheduleDate;
     await db.put(id, student);
     console.log(student);
 }
